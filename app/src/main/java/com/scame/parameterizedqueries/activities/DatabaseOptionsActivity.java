773b0b10d75eb1eade4d88e6d317c8284cf1dc31
Slava Petrochenko <vs.petrochenko@gmail.com>
2016-12-01 18:51:29 +0200

    implemented database options layout

---
+++ app/src/main/java/com/scame/parameterizedqueries/activities/DatabaseOptionsActivity.java
@@ -0,0 +1,15 @@
+package com.scame.parameterizedqueries.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.scame.parameterizedqueries.R;
+
+public class DatabaseOptionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.database_options);
+    }
+}
